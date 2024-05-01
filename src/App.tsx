b6b4f9fde0d599a65386a2cd7abcbd7d00232ae4
Owen Lynch <root@owenlynch.org>
2024-05-01 09:13:27 -0700

    added codemirror!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { HocuspocusProvider } from "@hocuspocus/provider";
-import { For, JSX, splitProps } from "solid-js";
-import { yArraySignal } from "./YSignal";
+import { HocuspocusProvider } from "@hocuspocus/provider"
+import { For, JSX, splitProps } from "solid-js"
+import { yArraySignal } from "./YSignal"
 import * as Y from "yjs"
+import * as random from "lib0/random"
+
+import { yCollab } from 'y-codemirror.next'
+import { EditorView, basicSetup } from "codemirror"
+import { EditorState } from "@codemirror/state"
 
 type Task = Y.Map<any> & {
   get(_: "description"): string
@@ -57,6 +62,20 @@ function Trash(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   )
 }
 
+export const usercolors = [
+  { color: '#30bced', light: '#30bced33' },
+  { color: '#6eeb83', light: '#6eeb8333' },
+  { color: '#ffbc42', light: '#ffbc4233' },
+  { color: '#ecd444', light: '#ecd44433' },
+  { color: '#ee6352', light: '#ee635233' },
+  { color: '#9ac2c9', light: '#9ac2c933' },
+  { color: '#8acb88', light: '#8acb8833' },
+  { color: '#1be7ff', light: '#1be7ff33' }
+]
+
+// select a random color for this user
+export const userColor = usercolors[random.uint32() % usercolors.length]
+
 function App() {
   // Connect it to the backend
   const provider = new HocuspocusProvider({
@@ -68,11 +87,37 @@ function App() {
 
   const ytasks: Y.Array<Task> = provider.document.getArray("tasks");
 
+  const ytext = provider.document.getText('codemirror')
+
+  const undoManager = new Y.UndoManager(ytext)
+
+  provider.awareness?.setLocalStateField('user', {
+    name: 'Anonymous ' + Math.floor(Math.random() * 100),
+    color: userColor.color,
+    colorLight: userColor.light
+  })
+
+  const state = EditorState.create({
+    doc: ytext.toString(),
+    extensions: [
+      basicSetup,
+      yCollab(ytext, provider.awareness, { undoManager })
+    ]
+  })
+
+  function onload(ref: Element) {
+    new EditorView({
+      state,
+      parent: ref
+    })
+  }
+
   return (
     <div class="font-sans container mx-auto">
       <h1 class="text-xl font-bold">Todo</h1>
       <TodoList ytasks={ytasks} />
       <Button onClick={_ => ytasks.push([newTask()])}>Add Todo</Button>
+      <div ref={onload}></div>
     </div>
   )
 }
