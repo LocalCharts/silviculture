671063cc5968772d437837f1cef4d1cd64746383
Owen Lynch <root@owenlynch.org>
2024-06-07 15:01:57 -0700

    updated yarn lock for nix package

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       let
         offlineCache = pkgs.fetchYarnDeps {
           yarnLock = ./yarn.lock;
-          hash = "sha256-CaYVmXGlIqghlfCgn9CYyoaokg5lcV18N6AFSYezsSk=";
+          hash = "sha256-DKrTfmQcOIlINvb7bCDCJbJAGr41Otww7YkyG1R8ClM=";
         };
         headers = pkgs.fetchurl {
           url = "https://nodejs.org/download/release/v20.11.1/node-v20.11.1-headers.tar.gz";
@@ -23,7 +23,7 @@
       in {
         default = pkgs.mkYarnPackage {
           pname = "silviculture";
-          version = "0.0.1";
+          version = "0.1.0";
           src = ./.;
           packageJSON = ./package.json;
           inherit offlineCache;
